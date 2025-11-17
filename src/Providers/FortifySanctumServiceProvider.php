8f4d199a213d4a5df5f44621e45daf76635f74c3
Ken van der Eerden <support+github@everware.nl>
2025-11-17 11:32:28 +0100

    Allow StatefulGuard to be overriden in certain situations

--- src/Providers/FortifySanctumServiceProvider.php
+++ src/Providers/FortifySanctumServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Everware\LaravelFortifySanctum\Providers;
 
 use Everware\LaravelFortifySanctum\Auth\Guards\FortifySanctumStatefulSessionGuard;
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -24,33 +25,62 @@ class FortifySanctumServiceProvider extends ServiceProvider
         \Auth::extend('fortify-sanctum', function(Application $app, string $name, array $config) {
             return new FortifySanctumStatefulSessionGuard(null, $name, $config);
         });
-        if (config('fortify.guard') == 'fortify-sanctum') {
-            /** {@see AuthenticatedSessionController::store()}. */
-            $this->app->bind(LoginRequest::class, function() {
-                return new class extends LoginRequest {
-                    public function rules() {
-                        return parent::rules() + ['device_name' => 'required'];
-                    }
-                };
-            });
-            /** {@see TwoFactorAuthenticatedSessionController::store()}. */
-            $this->app->bind(TwoFactorLoginRequest::class, function() {
-                return new class extends TwoFactorLoginRequest {
-                    public function rules() {
-                        return parent::rules() + ['device_name' => 'required'];
-                    }
-                };
-            });
-            /** {@see RegisteredUserController::store()}. TODO If Fortify ever creates a RegisterRequest like above, use that instead of this: */
-            $this->app->extend(CreatesNewUsers::class, function(CreatesNewUsers $action) {
-                return new class($action) implements CreatesNewUsers {
-                    public function __construct(protected CreatesNewUsers $parent){}
-                    public function create(array $input) {
+
+        /**
+         * No longer directly checking `if (config('fortify.guard') == 'fortify-sanctum')` here
+         * because other packages might have overriden StatefulGuard in certain situations.
+         * E.g. Nova Overwrites our StatefulGuard ({@see FortifySanctumServiceProvider::boot()} into {@see FortifyServiceProvider::register()})
+         * using `$app->scoped(StatefulGuard::class, static fn () => Auth::guard(Util::userGuard()));`
+         * in {@see PendingFortifyConfiguration::bootstrap()}, but only when `Nova::serving(...)` (like when posting to their login route).
+         */
+
+        /** {@see AuthenticatedSessionController::store()}. */
+        $this->app->bind(LoginRequest::class, function() {
+            return new class extends LoginRequest {
+                public function rules() {
+                    /**
+                     * See above `Auth::extend()` and
+                     * {@see FortifyServiceProvider::register()} (Fortify)
+                     * {@see PendingFortifyConfiguration::bootstrap()} (Nova).
+                     */
+                    return app(StatefulGuard::class) instanceof FortifySanctumStatefulSessionGuard
+                        ? parent::rules() + ['device_name' => 'required']
+                        : parent::rules();
+                }
+            };
+        });
+        /** {@see TwoFactorAuthenticatedSessionController::store()}. */
+        $this->app->bind(TwoFactorLoginRequest::class, function() {
+            return new class extends TwoFactorLoginRequest {
+                public function rules() {
+                    /**
+                     * See above `Auth::extend()` and
+                     * {@see FortifyServiceProvider::register()} (Fortify)
+                     * {@see PendingFortifyConfiguration::bootstrap()} (Nova).
+                     */
+                    return app(StatefulGuard::class) instanceof FortifySanctumStatefulSessionGuard
+                        ? parent::rules() + ['device_name' => 'required']
+                        : parent::rules();
+                }
+            };
+        });
+        /** {@see RegisteredUserController::store()}. TODO If Fortify ever creates a RegisterRequest like above, use that instead of this: */
+        $this->app->extend(CreatesNewUsers::class, function(CreatesNewUsers $action) {
+            return new class($action) implements CreatesNewUsers {
+                public function __construct(protected CreatesNewUsers $parent){}
+                public function create(array $input) {
+                    /**
+                     * See above `Auth::extend()` and
+                     * {@see FortifyServiceProvider::register()} (Fortify)
+                     * {@see PendingFortifyConfiguration::bootstrap()} (Nova).
+                     */
+                    if (app(StatefulGuard::class) instanceof FortifySanctumStatefulSessionGuard) {
                         validator($input, ['device_name' => 'required'])->validate();
-                        return $this->parent->create($input);
                     }
-                };
-            });
-        }
+
+                    return $this->parent->create($input);
+                }
+            };
+        });
     }
 }
